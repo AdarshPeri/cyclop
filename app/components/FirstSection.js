b29456d09bcda555331b0d70b251436fceea1cdf
adarshperi <adarshperi@gmail.com>
2024-06-16 12:29:32 +0530

    try 3

--- app/components/FirstSection.js
+++ app/components/FirstSection.js
@@ -13,12 +13,12 @@ export default function FirstSection() {
       <NavigationWithLogo />
       <FullPageNav />
       <Discover />
-      <div className='w-[100vw] bg-tertiary-200 relative mt-2 max-lg:mt-8 max-sm:mt-20'>
+      <div className='w-[100dvw] bg-tertiary-200 relative mt-2 max-lg:mt-8 max-sm:mt-20'>
         <Image src={union} alt='section-break' className='max-sm:hidden' />
-        <img
-          src='/sec1mob.svg'
+        <Image
+          src={section1}
           alt='section-break'
-          className='max-sm:block hidden w-[100dvw]'
+          className='max-sm:block hidden'
         />
         <p
           className={`${azeret.className} text-sm max-lg:text-xs w-[90%] lg:w-[80%] xl:w-[75%] absolute top-[30%] sm:max-lg:top-[10%] max-sm:-top-[30%] lg:max-xl:left-[10%] xl:left-[13%] left-[5%]`}
