79ef5d7adee69b77fe9fa320b8752007ab37e481
adarshperi <adarshperi@gmail.com>
2024-07-04 18:00:12 +0530

    added scroll and info for popovers

--- app/grow-your-brand/page.js
+++ app/grow-your-brand/page.js
@@ -22,32 +22,83 @@ export const subFeatures = [
   {
     title: 'office',
     features: [
-      'Logo Creation',
-      'Brand Identity Development',
-      'Basic Website Design',
-      'Foundational Analytics',
-      'Reporting On Customer Base & Marketing Trends',
-      'Personalized Attention & Creative Solutions',
+      {
+        featureName: 'Logo Creation',
+        featureDesc: `Let's cook up a logo that's as unique as your brand's secret sauce, something that sticks in minds and hearts!`,
+      },
+      {
+        featureName: 'Brand Identity Development',
+        featureDesc: `Think of us as your brand's personal stylists, crafting the perfect look with colors, fonts, and guidelines that scream "you" across every platform.`,
+      },
+      {
+        featureName: 'Basic Website Design',
+        featureDesc: `We'll whip up a website that's not just eye candy but also super easy to navigate, like a digital treasure map leading straight to your brand.`,
+      },
+      {
+        featureName: 'Foundational Analytics',
+        featureDesc: `We'll crunch the numbers so you can see who's loving your site and why—they're like digital detectives on the case!`,
+      },
+      {
+        featureName: 'Reporting On Customer Base & Marketing Trends',
+        featureDesc: `Our reports are like your brand's gossip column, dishing out juicy insights on who's buzzing about you and where the trends are heading.`,
+      },
+      {
+        featureName: 'Personalized Attention & Creative Solutions',
+        featureDesc: `We're not just your team, we're your partners in digital crime, dedicated to cracking the code on what makes your brand shine brighter than the rest!`,
+      },
     ],
   },
   {
     title: 'business',
     features: [
-      'Website Development',
-      'Marketing Collateral Creation',
-      'UX/UI (User Experience/User Interface) design',
-      `Advanced Data Services Like: Predictive analytics, Customer segmentation, Business intelligence reporting`,
-      'Close collaboration with clients & Driving operational efficiency',
+      {
+        featureName: 'Website Development',
+        featureDesc: `We're the architects behind websites that aren't just eye candy—they're functional and super flexible, like acrobats doing back-flips on any device. Our mission? Making your online presence not just look good, but work flawlessly too!`,
+      },
+      {
+        featureName: 'Marketing Collateral Creation',
+        featureDesc: `We love crafting brochures, flyers, and posters that really tell your brand's story. Our goal is to make your message pop and resonate with your audience, boosting your marketing efforts in a way that feels just right.`,
+      },
+      {
+        featureName: 'UX/UI (User Experience/User Interface) design',
+        featureDesc: `We specialize in creating interfaces that are not only intuitive but also irresistibly user-friendly. Our designs are like magic spells that enhance user experience, keeping your audience engaged and delighted with every click and swipe.`,
+      },
+      {
+        featureName: `Advanced Data Services: Business intelligence reporting, Predictive analytics, Customer segmentation`,
+        featureDesc: `We predict future trends with predictive analytics, giving you a competitive edge. Our customer segmentation sorts clients into ideal groups based on preferences, delivering personalized experiences. Through business intelligence reporting, we decode data to uncover insights, revealing your business's strengths and areas for growth.`,
+      },
+      {
+        featureName:
+          'Close collaboration with clients & Driving operational efficiency',
+        featureDesc: `We're your partners in crime for business brilliance! Together, we dive deep into your goals and challenges, crafting solutions that turbo-charge efficiency, slash costs, and amp up productivity. Let's make your business a lean, mean, efficiency machine!`,
+      },
     ],
   },
   {
     title: 'pro',
     features: [
-      'Custom App Development',
-      'Interactive Media Design',
-      'Strategic Branding Initiatives',
-      'Holistic Data Services',
-      'Optimizing operational performance & delivering high-impact solutions',
+      {
+        featureName: 'Custom App Development',
+        featureDesc: `Expect premium design services in creating apps that feel like they were made just for you — slick, intuitive, and ready to impress your users.`,
+      },
+      {
+        featureName: 'Interactive Media Design',
+        featureDesc: `Get ready for eye-popping animations and engaging videos that make your brand sparkle and your audience say "wow!"`,
+      },
+      {
+        featureName: 'Strategic Branding Initiatives',
+        featureDesc: `We'll cook up branding strategies so good, your competitors will wonder how you make it look so easy.`,
+      },
+      {
+        featureName:
+          'Holistic Data Services: Machine learning models, Big data analytics, Tailored insights',
+        featureDesc: `Our machine learning models act like your business's secret sauce—predicting trends, automating decisions, and boosting operational efficiency. Dive into the vast sea of big data with us to uncover hidden insights that transform challenges into victories. Think of us as your business detectives, finding tailored solutions to complex problems and guiding you confidently toward success.`,
+      },
+      {
+        featureName:
+          'Optimizing operational performance & delivering high-impact solutions',
+        featureDesc: `Think of us as your business's personal efficiency gurus — streamlining processes, cutting costs, and making everything run smoother than a well-oiled machine.We bring serious know-how and creativity to the table & craft goal oriented solutions.`,
+      },
     ],
   },
 ];
@@ -128,8 +179,8 @@ const Plan = ({ subFeature: { title, features } }) => {
               className='flex justify-between border-b-alternate-50/10 border-b-2 border-dashed mb-4 pb-4 last:border-none'
               key={index}
             >
-              <p className='text-sm pr-1'>{feature}</p>
-              <ReturnQuestionSvg />
+              <p className='text-sm pr-1'>{feature.featureName}</p>
+              <ReturnQuestionSvg desc={feature.featureDesc} />
             </div>
           ))}
         </div>
@@ -144,27 +195,28 @@ const Plan = ({ subFeature: { title, features } }) => {
   );
 };
 
-export const ReturnQuestionSvg = ({ fill = '#202023' }) => {
+export const ReturnQuestionSvg = ({ desc }) => {
   return (
-    <div className='relative group/q'>
-      <svg
-        width='24'
-        height='24'
-        viewBox='0 0 24 24'
-        fill='none'
-        xmlns='http://www.w3.org/2000/svg'
-      >
-        <path
-          id='Vector'
-          d='M13.3846 18C13.3846 18.2738 13.3034 18.5415 13.1513 18.7692C12.9991 18.9969 12.7829 19.1744 12.5299 19.2792C12.2769 19.384 11.9985 19.4114 11.7299 19.358C11.4613 19.3046 11.2146 19.1727 11.0209 18.9791C10.8273 18.7854 10.6954 18.5387 10.642 18.2701C10.5886 18.0015 10.616 17.7231 10.7208 17.4701C10.8256 17.2171 11.0031 17.0009 11.2308 16.8487C11.4585 16.6966 11.7262 16.6154 12 16.6154C12.3672 16.6154 12.7194 16.7613 12.9791 17.0209C13.2387 17.2806 13.3846 17.6328 13.3846 18ZM12 5.53846C9.45462 5.53846 7.38462 7.40192 7.38462 9.69231V10.1538C7.38462 10.3987 7.48187 10.6334 7.65498 10.8066C7.82809 10.9797 8.06288 11.0769 8.3077 11.0769C8.55251 11.0769 8.7873 10.9797 8.96041 10.8066C9.13352 10.6334 9.23077 10.3987 9.23077 10.1538V9.69231C9.23077 8.42308 10.4735 7.38461 12 7.38461C13.5265 7.38461 14.7692 8.42308 14.7692 9.69231C14.7692 10.9615 13.5265 12 12 12C11.7552 12 11.5204 12.0972 11.3473 12.2704C11.1742 12.4435 11.0769 12.6783 11.0769 12.9231V13.8462C11.0769 14.091 11.1742 14.3258 11.3473 14.4989C11.5204 14.672 11.7552 14.7692 12 14.7692C12.2448 14.7692 12.4796 14.672 12.6527 14.4989C12.8258 14.3258 12.9231 14.091 12.9231 13.8462V13.7631C15.0277 13.3765 16.6154 11.6977 16.6154 9.69231C16.6154 7.40192 14.5454 5.53846 12 5.53846ZM24 12C24 14.3734 23.2962 16.6934 21.9776 18.6668C20.6591 20.6402 18.7849 22.1783 16.5922 23.0865C14.3995 23.9948 11.9867 24.2324 9.65892 23.7694C7.33115 23.3064 5.19295 22.1635 3.51472 20.4853C1.83649 18.807 0.693605 16.6689 0.230582 14.3411C-0.232441 12.0133 0.00519941 9.60051 0.913451 7.4078C1.8217 5.21508 3.35977 3.34094 5.33316 2.02236C7.30655 0.703788 9.62663 0 12 0C15.1816 0.00335979 18.2319 1.26872 20.4816 3.51843C22.7313 5.76814 23.9966 8.81843 24 12ZM22.1538 12C22.1538 9.99176 21.5583 8.02861 20.4426 6.35882C19.3269 4.68903 17.7411 3.38759 15.8857 2.61907C14.0303 1.85055 11.9887 1.64947 10.0191 2.04126C8.04943 2.43305 6.24019 3.40011 4.82015 4.82015C3.40011 6.24019 2.43305 8.04943 2.04126 10.0191C1.64947 11.9887 1.85055 14.0303 2.61907 15.8857C3.38759 17.7411 4.68904 19.3269 6.35883 20.4426C8.02862 21.5583 9.99176 22.1538 12 22.1538C14.692 22.1508 17.2729 21.08 19.1765 19.1765C21.08 17.2729 22.1508 14.692 22.1538 12Z'
-          fill='#202023'
-        />
-      </svg>
+    <div className='relative group'>
+      <div className=''>
+        <svg
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            id='Vector'
+            d='M13.3846 18C13.3846 18.2738 13.3034 18.5415 13.1513 18.7692C12.9991 18.9969 12.7829 19.1744 12.5299 19.2792C12.2769 19.384 11.9985 19.4114 11.7299 19.358C11.4613 19.3046 11.2146 19.1727 11.0209 18.9791C10.8273 18.7854 10.6954 18.5387 10.642 18.2701C10.5886 18.0015 10.616 17.7231 10.7208 17.4701C10.8256 17.2171 11.0031 17.0009 11.2308 16.8487C11.4585 16.6966 11.7262 16.6154 12 16.6154C12.3672 16.6154 12.7194 16.7613 12.9791 17.0209C13.2387 17.2806 13.3846 17.6328 13.3846 18ZM12 5.53846C9.45462 5.53846 7.38462 7.40192 7.38462 9.69231V10.1538C7.38462 10.3987 7.48187 10.6334 7.65498 10.8066C7.82809 10.9797 8.06288 11.0769 8.3077 11.0769C8.55251 11.0769 8.7873 10.9797 8.96041 10.8066C9.13352 10.6334 9.23077 10.3987 9.23077 10.1538V9.69231C9.23077 8.42308 10.4735 7.38461 12 7.38461C13.5265 7.38461 14.7692 8.42308 14.7692 9.69231C14.7692 10.9615 13.5265 12 12 12C11.7552 12 11.5204 12.0972 11.3473 12.2704C11.1742 12.4435 11.0769 12.6783 11.0769 12.9231V13.8462C11.0769 14.091 11.1742 14.3258 11.3473 14.4989C11.5204 14.672 11.7552 14.7692 12 14.7692C12.2448 14.7692 12.4796 14.672 12.6527 14.4989C12.8258 14.3258 12.9231 14.091 12.9231 13.8462V13.7631C15.0277 13.3765 16.6154 11.6977 16.6154 9.69231C16.6154 7.40192 14.5454 5.53846 12 5.53846ZM24 12C24 14.3734 23.2962 16.6934 21.9776 18.6668C20.6591 20.6402 18.7849 22.1783 16.5922 23.0865C14.3995 23.9948 11.9867 24.2324 9.65892 23.7694C7.33115 23.3064 5.19295 22.1635 3.51472 20.4853C1.83649 18.807 0.693605 16.6689 0.230582 14.3411C-0.232441 12.0133 0.00519941 9.60051 0.913451 7.4078C1.8217 5.21508 3.35977 3.34094 5.33316 2.02236C7.30655 0.703788 9.62663 0 12 0C15.1816 0.00335979 18.2319 1.26872 20.4816 3.51843C22.7313 5.76814 23.9966 8.81843 24 12ZM22.1538 12C22.1538 9.99176 21.5583 8.02861 20.4426 6.35882C19.3269 4.68903 17.7411 3.38759 15.8857 2.61907C14.0303 1.85055 11.9887 1.64947 10.0191 2.04126C8.04943 2.43305 6.24019 3.40011 4.82015 4.82015C3.40011 6.24019 2.43305 8.04943 2.04126 10.0191C1.64947 11.9887 1.85055 14.0303 2.61907 15.8857C3.38759 17.7411 4.68904 19.3269 6.35883 20.4426C8.02862 21.5583 9.99176 22.1538 12 22.1538C14.692 22.1508 17.2729 21.08 19.1765 19.1765C21.08 17.2729 22.1508 14.692 22.1538 12Z'
+            fill='#202023'
+          />
+        </svg>
+      </div>
       <p
-        className={`-right-[85%] clip-pop-right absolute hidden -top-[175px] bg-black px-3 pt-4 pb-8 w-80 group-hover/q:block group-hover:brightness-100 z-50 text-white`}
+        className={`-right-[85%] clip-pop-right absolute hidden -top-[175px] bg-black px-3 pt-4 pb-8 w-80 group-hover:block group-hover:brightness-100 z-50 text-white h-40 overflow-y-scroll`}
       >
-        Pellentesque sit amet sapien fringilla. Aliquam in hendrerit urna.Ut et
-        massa mi.Pellentesque sit amet sapien fringilla.
+        {desc}
       </p>
     </div>
   );
